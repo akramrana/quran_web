78c3bc7c15084ebd9e7e45d87dc55ece3ee11143
Akram Hossain <akram686202@gmail.com>
2023-02-16 11:50:00 +0600

    changes

--- src/app/pages/quran/ayah-tafsir/ayah-tafsir.component.ts
+++ src/app/pages/quran/ayah-tafsir/ayah-tafsir.component.ts
@@ -5,6 +5,7 @@ import { first } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ayah-tafsir',
@@ -18,6 +19,10 @@ export class AyahTafsirComponent implements OnInit {
   tafsir: any = {};
   @Input() fileToPlay: string = '';
   showPlayer: boolean = false;
+  formGroup: FormGroup | any;
+  ayahList: any[] = [];
+  surahList: any[] = [];
+  selectedSurah: any;
 
   constructor(
     private domSanitizer: DomSanitizer,
@@ -27,9 +32,14 @@ export class AyahTafsirComponent implements OnInit {
     private cdr: ChangeDetectorRef,
     private toastr: ToastrService,
     private router: Router,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      surahId: [''],
+      ayahId: [''],
+    })
 
     this._route.params.subscribe((param) => {
       this.surahId = param['surahId'];
@@ -44,11 +54,26 @@ export class AyahTafsirComponent implements OnInit {
           const data = response.body.data;
           if (data) {
             this.tafsir = data;
-
-            this.titleService.setTitle(this.tafsir.name_complex+":"+this.ayahId);
+            this.titleService.setTitle(this.tafsir.name_complex + ":" + this.ayahId);
+            this.showPlayer = true;
+            this.cdr.detectChanges();
+            var audio = document.getElementById('audio') as HTMLAudioElement;
+            var source = document.getElementById('audioSource') as HTMLSourceElement;
+            source.src = this.tafsir.audio_url;
+            audio.load();
           }
         });
-    })
+    });
+
+    this.apiService.getSurahList({})
+      .pipe(first())
+      .subscribe(response => {
+        const data = response.body.list;
+        //console.log(data);
+        if (data) {
+          this.surahList = data;
+        }
+      });
   }
 
   goBack() {
@@ -61,6 +86,41 @@ export class AyahTafsirComponent implements OnInit {
 
   nextAyah() {
     this.router.navigate(['/pages/quran/tafsir/' + this.tafsir.surah_id + '/' + this.tafsir.next_ayah]);
+  }
+
+  setAyah() {
+    this.ayahList = [];
+    this.formGroup.patchValue({
+      ayahId: ""
+    });
+    if (this.selectedSurah) {
+      let totalAyah = Number(this.selectedSurah.ayat);
+      for (let i = 1; i <= totalAyah; i++) {
+        let d = {
+          value: i,
+          name: i,
+        }
+        this.ayahList.push(d);
+      }
+    }
+  }
+
+  search() {
+    Object.keys(this.formGroup.controls).forEach(field => {
+      const control = this.formGroup.get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
+    if (this.formGroup.valid) {
+      try {
+        const postParams = this.formGroup.value;
+        let surah_id = postParams.surahId.surah_id;
+        let ayah_num = postParams.ayahId.value;
+        //
+        this.router.navigate(['/pages/quran/tafsir/' + surah_id + '/' + ayah_num]);
+      } catch (e) {
+        console.log(e);
+      }
+    }
   }
 
 }
