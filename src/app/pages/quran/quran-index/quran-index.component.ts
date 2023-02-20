3dd191d77f7f14c33e969473d21dbf95fba5846e
Akram Hossain <akram686202@gmail.com>
2023-02-20 22:16:01 +0600

    changes

--- src/app/pages/quran/quran-index/quran-index.component.ts
+++ src/app/pages/quran/quran-index/quran-index.component.ts
@@ -26,6 +26,51 @@ export class QuranIndexComponent implements OnInit {
 
   formGroup: FormGroup | any;
 
+  kitabList: any = [
+    {
+      id: 5,
+      nameEn: "Sahih al-Bukhari",
+      nameBn: "সহীহ আল-বুখারী",
+      nameAr: "صحيح البخاري",
+      nameSlug: "bukhari"
+    },
+    {
+      id: 6,
+      nameEn: "Sahih Muslim",
+      nameBn: "সহীহ মুসলিম",
+      nameAr: "صحيح مسلم",
+      nameSlug: "muslim"
+    },
+    {
+      id: 1,
+      nameEn: "Sunan an-Nasa'i",
+      nameBn: "সুনানে নাসাঈ",
+      nameAr: "سنن النسائي",
+      nameSlug: "nasai"
+    },
+    {
+      id: 2,
+      nameEn: "Sunan Abi Dawud",
+      nameBn: "সুনানে আবু দাউদ",
+      nameAr: "سنن أبي داود",
+      nameSlug: "abudawud"
+    },
+    {
+      id: 4,
+      nameEn: "Jami` at-Tirmidhi",
+      nameBn: "জামিউত তিরমিজী",
+      nameAr: "جامع الترمذي ",
+      nameSlug: "tirmidhi"
+    },
+    {
+      id: 3,
+      nameEn: "Sunan Ibn Majah",
+      nameBn: "সুনানে ইবনে মাজাহ",
+      nameAr: "سنن ابن ماجه",
+      nameSlug: "ibnmajah"
+    },
+  ];
+
   constructor(
     private domSanitizer: DomSanitizer,
     private apiService: ApiService,
