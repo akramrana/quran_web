fd8aada2c52a7222037fbce63f1409ee5aaee574
Akram Hossain <akram686202@gmail.com>
2023-11-29 12:29:43 +0600

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { SeoService } from './services/seo.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quran';
+
+  constructor(
+    private router: Router,
+    private seoService: SeoService,
+    @Inject(DOCUMENT) private document: any,
+  ) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+      }
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+        //console.log(event.url);
+        this.destroyLinkForCanonicalURL();
+      }
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+        // Present error to user
+        console.log(event.error);
+      }
+    });
+  }
+
+  createLinkForCanonicalURL() {
+    this.seoService.createLinkForCanonicalURL();
+  }
+
+  destroyLinkForCanonicalURL() {
+    const els = this.document.querySelectorAll('link[rel=\'canonical\']');
+    for (let i = 0, l = els.length; i < l; i++) {
+      const el = els[i];
+      el.remove();
+    }    
+    this.createLinkForCanonicalURL();
+  }
 }
